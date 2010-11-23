21bcc53bea5b7ba8643adb6d16aa8e5851549685
Daniel Lienert <daniel@lienert.cc>
2010-11-23 09:30:11 +0100

    INIT

---
+++ ext_localconf.php
@@ -0,0 +1,15 @@
+<?php
+if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
+
+Tx_Extbase_Utility_Extension::configurePlugin(
+	$_EXTKEY,
+	'Pi1',
+	array(
+		
+	),
+	array(
+		
+	)
+);
+
+?>
---
+++ ext_tables.php
@@ -0,0 +1,16 @@
+<?php
+if (!defined ('TYPO3_MODE')) die ('Access denied.');
+
+Tx_Extbase_Utility_Extension::registerPlugin(
+	$_EXTKEY,
+	'Pi1',
+	'Extlist special'
+);
+
+t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Extlist special');
+
+//$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY . '_pi1'] = 'pi_flexform';
+//t3lib_extMgm::addPiFlexFormValue($_EXTKEY . '_pi1', 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_list.xml');
+
+
+?>
