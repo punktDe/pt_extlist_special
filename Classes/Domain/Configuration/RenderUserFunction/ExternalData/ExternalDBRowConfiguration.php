de412e15091cca477623ab94fde2807b78f97613
Simon Schaufelberger <schaufelberger@punkt.de>
2010-12-07 19:46:45 +0100

    ADD: replaceQueryValuesRecursive to recursively replace markers

--- Classes/Domain/Configuration/RenderUserFunction/ExternalData/ExternalDBRowConfiguration.php
+++ Classes/Domain/Configuration/RenderUserFunction/ExternalData/ExternalDBRowConfiguration.php
@@ -31,76 +31,76 @@
  * @subpackage Configuration\RenderUserFunction\ExternalData
  * @author Daniel Lienert <lienert@punkt.de>
  */
-class Tx_PtExtlistSpecial_Domain_Configuration_RenderUserFunction_ExternalData_ExternalDBRowConfiguration  extends Tx_PtExtlist_Domain_Configuration_AbstractExtlistConfiguration 
+class Tx_PtExtlistSpecial_Domain_Configuration_RenderUserFunction_ExternalData_ExternalDBRowConfiguration  extends Tx_PtExtlist_Domain_Configuration_AbstractExtlistConfiguration
 																										   implements Tx_PtExtlist_Domain_Configuration_RenderConfigInterface {
-	
-	/**	
+
+	/**
 	 * @var string
 	 */
 	protected $dsn;
-	
-	
-	/**	
+
+
+	/**
 	 * @var string
 	 */
 	protected $user;
-	
-	
-	/**	
+
+
+	/**
 	 * @var string
 	 */
 	protected $password;
-	
-	
-	/**	
+
+
+	/**
 	 * @var string
 	 */
 	protected $select;
-	
-	
-	/**	
+
+
+	/**
 	 * @var string
 	 */
 	protected $from;
-	
-	
-	/**	
+
+
+	/**
 	 * @var string
 	 */
 	protected $where;
-	
-	
-	/**	
+
+
+	/**
 	 * @var string
 	 */
 	protected $orderBy;
-	
-	
-	/**	
+
+
+	/**
 	 * @var string
 	 */
 	protected $groupBy;
-	
-	
-	/**	
+
+
+	/**
 	 * @var array
 	 */
 	protected $renderObj;
-	
-	
-	/**	
+
+
+	/**
 	 * @var string
 	 */
 	protected $renderTemplate;
-	
-	
-	
+
+
+
 	/**
 	 * @var array
 	 */
 	protected $renderUserFunctions;
-	
-	
+
+
 	/**
 	 * @param array $settings
 	 * @param array $internalFieldData
@@ -109,11 +109,11 @@ class Tx_PtExtlistSpecial_Domain_Configuration_RenderUserFunction_ExternalData_E
 		$this->settings = $this->prepareSettingsForSpecificRow($settings, $internalFieldData);
 		$this->init();
 	}
-	
-	
+
+
 	/**
 	 * Fill placeholders in query part with data from the current row
-	 * 
+	 *
 	 * @param array $settings
 	 * @param array $internalFieldData
 	 */
@@ -123,19 +123,41 @@ class Tx_PtExtlistSpecial_Domain_Configuration_RenderUserFunction_ExternalData_E
 		foreach ($internalFieldData as $key => $value) {
 			$replaceArray['###'.$key.'###'] = $value;
 		}
-		
+
 		foreach ($settings['query'] as $queryPart => $queryValue) {
-			// resolve stdWraps
-			$value = Tx_PtExtlist_Utility_RenderValue::stdWrapIfPlainArray($queryValue);
 			// insert values
-			$value = str_replace(array_keys($replaceArray), array_values($replaceArray), $value);
+			$value = $this->replaceQueryValuesRecursive($queryValue, $replaceArray);
+
+			// resolve stdWraps
+			$value = Tx_PtExtlist_Utility_RenderValue::stdWrapIfPlainArray($value);
 			$settings[$queryPart] = $value;
 		}
-		
+
 		return $settings;
 	}
-	
-	
+
+
+
+	/**
+	 * Replace string in array recursive
+	 *
+	 * @param mixed $queryValue
+	 * @param array $replaceArray
+	 * @return mixed
+	 */
+	protected function replaceQueryValuesRecursive($queryValue, &$replaceArray) {
+		if (!is_array($queryValue)) {
+			return str_replace(array_keys($replaceArray), array_values($replaceArray), $queryValue);
+		}
+
+		foreach ($queryValue as $key => $value) {
+			$queryValue[$key] = $this->replaceQueryValuesRecursive($value, $replaceArray);
+		}
+		return $queryValue;
+	}
+
+
+
 	/**
 	 * (non-PHPdoc)
 	 * @see Classes/Domain/Configuration/Tx_PtExtlist_Domain_Configuration_AbstractConfiguration::init()
@@ -144,66 +166,66 @@ class Tx_PtExtlistSpecial_Domain_Configuration_RenderUserFunction_ExternalData_E
 		$this->setRequiredValue('dsn', 'No DSN Configuration set for ExternalDBRow Render User Function! 1290506596');
 		$this->setValueIfExistsAndNotNothing('user');
 		$this->setValueIfExistsAndNotNothing('password');
-		
+
 		$this->setRequiredValue('select', 'No select part given for ExternalDBRow Render User Function! 1290507801');
 		$this->setValueIfExistsAndNotNothing('from');
 		$this->setValueIfExistsAndNotNothing('where');
 		$this->setValueIfExistsAndNotNothing('groupBy');
 		$this->setValueIfExistsAndNotNothing('orderBy');
-		
+
 		$this->setValueIfExistsAndNotNothing('renderTemplate');
 		if(array_key_exists('renderUserFunctions', $this->settings) && is_array($this->settings['renderUserFunctions'])) {
 			asort($this->settings['renderUserFunctions']);
 			$this->renderUserFunctions = $this->settings['renderUserFunctions'];
 		}
-	
+
 		if(array_key_exists('renderObj', $this->settings) && $this->settings['renderObj']) {
-        	$this->renderObj = Tx_Extbase_Utility_TypoScript::convertPlainArrayToTypoScriptArray(array('renderObj' => $this->settings['renderObj']));
-        }
+			$this->renderObj = Tx_Extbase_Utility_TypoScript::convertPlainArrayToTypoScriptArray(array('renderObj' => $this->settings['renderObj']));
+		}
 	}
-	
+
 	public function getDSN() {
-		return $this->dsn;	
+		return $this->dsn;
 	}
-	
+
 	public function getUser() {
 		return $this->user;
 	}
-	
+
 	public function getPassword() {
 		return $this->password;
 	}
-	
+
 	public function getSelect() {
 		return $this->select;
 	}
-	
+
 	public function getFrom() {
 		return $this->from;
 	}
-	
+
 	public function getWhere() {
 		return $this->where;
 	}
-	
+
 	public function getOrderBy() {
 		return $this->orderBy;
 	}
-	
+
 	public function getGroupBy() {
 		return $this->groupBy;
 	}
-	
-	
+
+
 	/**
-	 * Limit is hardcoded to 1 for the externla DB row userfunction 
+	 * Limit is hardcoded to 1 for the externla DB row userfunction
 	 */
 	public function getLimit() {
 		return 1;
 	}
-	
-	
-	
+
+
+
 	/**
 	 * (non-PHPdoc)
 	 * @see Classes/Domain/Configuration/Tx_PtExtlist_Domain_Configuration_RenderConfigInterface::getRenderObj()
@@ -211,29 +233,29 @@ class Tx_PtExtlistSpecial_Domain_Configuration_RenderUserFunction_ExternalData_E
 	public function getRenderObj() {
 		return $this->renderObj;
 	}
-	
-	
-	
+
+
+
 	/**
 	 * Returns a configuration array for user functions
-	 * 
+	 *
 	 * @return array userFunctions Configuration
 	 */
 	public function getRenderUserFunctions() {
 		return $this->renderUserFunctions;
 	}
-	
-	
-	
+
+
+
 	/**
 	 * Returns a path to a fluid template file
-	 * 
+	 *
 	 * @returns string template
 	 */
 	public function getRenderTemplate() {
 		return $this->renderTemplate;
 	}
-	
-	
+
+
 }
 ?>
