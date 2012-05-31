fb1a2a5beac4b3d60ff7bc316dc75b0563a6d398
Christian Herberger <herberger@punkt.de>
2012-05-31 17:09:11 +0200

    ADD: prefix for static template

--- ext_tables.php
+++ ext_tables.php
@@ -9,9 +9,9 @@ Tx_Extbase_Utility_Extension::registerPlugin(
 );
 */
 
-t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Extlist special Base');
-t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript/Export', 'Extlist special Exports');
-t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript/Demolist', 'Extlist special Demos');
+t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', '[pt_special] Extlist special Base');
+t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript/Export', '[pt_special] Extlist special Exports');
+t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript/Demolist', '[pt_special] Extlist special Demos');
 
 //$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY . '_pi1'] = 'pi_flexform';
 //t3lib_extMgm::addPiFlexFormValue($_EXTKEY . '_pi1', 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_list.xml');
